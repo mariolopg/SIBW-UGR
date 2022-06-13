1afbf05345463ed4fab00a76b01c8f211cd5897a
Mario López González <mariolopezgonzalez2001@gmail.com>
2022-06-13 17:06:35 +0200

    upload sql

--- P4-P5/docker-compose-lamp/www/sql/sneakers.sql
+++ P4-P5/docker-compose-lamp/www/sql/sneakers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql:3306
--- Generation Time: Jun 02, 2022 at 12:01 PM
+-- Generation Time: Jun 13, 2022 at 03:05 PM
 -- Server version: 8.0.28
 -- PHP Version: 8.0.15
 
@@ -123,7 +123,7 @@ INSERT INTO `sneakersImages` (`id`, `id_sneaker`, `image_name`) VALUES
 (11, 10, 'dunk_1.png'),
 (12, 11, 'dunk_2.png'),
 (13, 12, 'dunk_3.png'),
-(14, 1, 'jordan_1_3.png');
+(25, 1, 'jordan_1_3.png');
 
 -- --------------------------------------------------------
 
@@ -136,26 +136,27 @@ CREATE TABLE `sneakersInfo` (
   `name` varchar(30) NOT NULL DEFAULT 'no_name',
   `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `precio` int NOT NULL,
-  `valoraciones` int NOT NULL
+  `valoraciones` int NOT NULL,
+  `estado` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'publicado'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `sneakersInfo`
 --
 
-INSERT INTO `sneakersInfo` (`id`, `name`, `description`, `precio`, `valoraciones`) VALUES
-(1, 'Jordan X Dior', 'La Air Jordan 1 X Dior son unas de las colaboraciones más exclusivas dentro del mundo de la moda.', 7485, 150),
-(2, 'Jordan Mid Lanen', 'La zapatilla Air Jordan 1 Mid está inspirada en la primera AJ1 y supone toda una oportunidad para los aficionados de las zapatillas Jordan clásicas de seguir los grandiosos pasos de su ídolo. El color vivo hace relucir los materiales elegantemente clásicos y aporta todo un toque innovador al conocido diseño.', 193, 58),
-(3, 'Jordan Light Blue', 'Zapatillas Jordan 1 en color azul claro.', 120, 243),
-(4, 'Jordan Tie Dye', 'Jordan 1 con estampado TieDye', 256, 12),
-(5, 'Jordan X A Ma Maniére', 'La tienda de James Whitner en Atlanta A Ma Maniére es conocida por combinar el lujo de la alta costura con un gran sentido de la finalidad. En esta versión de las Air Jordan 1, la tienda se adentra en los mitos sobre MJ para crear un modelo a la altura de la leyenda.', 532, 24),
-(6, 'Jordan Light Iron Ore', 'Las Air Jordan 1 Mid SE Light Iron Ore presentan una base de cuero blanco granulado con superposiciones de lona gruesa de color gris. Un swoosh de cuero rosa adorna los paneles y proporciona contraste a la silueta neutra. Las marcas de la firma, como el logotipo Jumpman y Wings, presentan un tono plateado que combina cuidadosamente con la entresuela blanca y la suela gris.', 125, 89),
-(7, 'AF1 Classic', 'El fulgor sigue vivo con las Nike Air Force 1 \'07, un icono del baloncesto que aporta un nuevo toque a su ya característica piel impecable, sus colores llamativos y la cantidad perfecta de reflectante.', 100, 1329),
-(8, 'AF1 University Blue', 'El clásico modelo AF1 con unos unos tonos azules.', 150, 512),
-(9, 'AF1 Psychic Blue', 'El clásico modelo AF1 con unos unos tonos azules claros.', 150, 183),
-(10, 'Dunk Low Essential Paisley', 'Dunk Low en tonos verdes.', 281, 238),
-(11, 'Dunk Low StrangeLove', 'Dunk Low en tonos rojos', 678, 239),
-(12, 'Dunk Low Green Glow', 'Dunk Low en tonos verde neón.', 248, 67);
+INSERT INTO `sneakersInfo` (`id`, `name`, `description`, `precio`, `valoraciones`, `estado`) VALUES
+(1, 'Jordan X Dior', 'La Air Jordan 1 X Dior son unas de las colaboraciones más exclusivas dentro del mundo de la moda.', 7485, 150, 'sin publicar'),
+(2, 'Jordan Mid Lanen', 'La zapatilla Air Jordan 1 Mid está inspirada en la primera AJ1 y supone toda una oportunidad para los aficionados de las zapatillas Jordan clásicas de seguir los grandiosos pasos de su ídolo. El color vivo hace relucir los materiales elegantemente clásicos y aporta todo un toque innovador al conocido diseño.', 193, 58, 'publicado'),
+(3, 'Jordan Light Blue', 'Zapatillas Jordan 1 en color azul claro.', 120, 243, 'publicado'),
+(4, 'Jordan Tie Dye', 'Jordan 1 con estampado TieDye', 256, 12, 'publicado'),
+(5, 'Jordan X A Ma Maniére', 'La tienda de James Whitner en Atlanta A Ma Maniére es conocida por combinar el lujo de la alta costura con un gran sentido de la finalidad. En esta versión de las Air Jordan 1, la tienda se adentra en los mitos sobre MJ para crear un modelo a la altura de la leyenda.', 532, 24, 'publicado'),
+(6, 'Jordan Light Iron Ore', 'Las Air Jordan 1 Mid SE Light Iron Ore presentan una base de cuero blanco granulado con superposiciones de lona gruesa de color gris. Un swoosh de cuero rosa adorna los paneles y proporciona contraste a la silueta neutra. Las marcas de la firma, como el logotipo Jumpman y Wings, presentan un tono plateado que combina cuidadosamente con la entresuela blanca y la suela gris.', 125, 89, 'publicado'),
+(7, 'AF1 Classic', 'El fulgor sigue vivo con las Nike Air Force 1 \'07, un icono del baloncesto que aporta un nuevo toque a su ya característica piel impecable, sus colores llamativos y la cantidad perfecta de reflectante.', 100, 1329, 'publicado'),
+(8, 'AF1 University Blue', 'El clásico modelo AF1 con unos unos tonos azules.', 150, 512, 'publicado'),
+(9, 'AF1 Psychic Blue', 'El clásico modelo AF1 con unos unos tonos azules claros.', 150, 183, 'publicado'),
+(10, 'Dunk Low Essential Paisley', 'Dunk Low en tonos verdes.', 281, 238, 'publicado'),
+(11, 'Dunk Low StrangeLove', 'Dunk Low en tonos rojos', 678, 239, 'publicado'),
+(12, 'Dunk Low Green Glow', 'Dunk Low en tonos verde neón.', 248, 67, 'publicado');
 
 -- --------------------------------------------------------
 
@@ -176,7 +177,8 @@ CREATE TABLE `tags` (
 INSERT INTO `tags` (`id`, `tag`, `id_sneaker`) VALUES
 (1, 'nike', 1),
 (3, 'dior', 1),
-(4, 'baloncesto', 1);
+(4, 'baloncesto', 1),
+(8, 'basket', 1);
 
 -- --------------------------------------------------------
 
@@ -197,11 +199,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nickname`, `email`, `password`, `rol`) VALUES
-(1, 'mariolopg', 'mario@gmail.com', '$2y$10$38tuxnFgFTzlBScsD6dxSOhdy7rTKBU8bw9PrzSRj05i/69C43VOO', 'superuser'),
-(9, 'admin', 'admin@admin.com', '$2y$10$UwaE4cN/BYwK50BOI16jCOSzQD2wDfUhEK5vn8ZthXVH1qxZDfg72', 'superuser'),
-(15, 'default', 'default@default.com', '$2y$10$3bSPEwVlcucc7SRfTamJju1qCvaozsZrdzAReLb9tVsIJDYQkbTHC', 'default'),
+(1, 'mariolopg', 'mariolopg@gmail.com', '$2y$10$38tuxnFgFTzlBScsD6dxSOhdy7rTKBU8bw9PrzSRj05i/69C43VOO', 'superuser'),
+(9, 'admin', 'administrador@gmail.com', '$2y$10$UwaE4cN/BYwK50BOI16jCOSzQD2wDfUhEK5vn8ZthXVH1qxZDfg72', 'superuser'),
 (16, 'moderador', 'moderador@moderador.com', '$2y$10$CjMtjfTfIkb3ZkiE61FLleOdzHd1VLvvXS/eILOUn1.9Hfu1D3NZS', 'moderador'),
-(17, 'gestor', 'gestor@gestor.com', '$2y$10$oS1V3qButAD4JZ7jENXY/O8ogUAqWPSF7hDpP/460OTQpyjB3JRlG', 'gestor');
+(17, 'gestor', 'gestor@gestor.com', '$2y$10$oS1V3qButAD4JZ7jENXY/O8ogUAqWPSF7hDpP/460OTQpyjB3JRlG', 'gestor'),
+(18, 'default', 'default@default.com', '$2y$10$Z4ZpZXt.3mn79QM3bH54Mu2BSRaveVtiP587qFXgiyzSvwoxDjxt2', 'default');
 
 --
 -- Indexes for dumped tables
@@ -262,31 +264,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `sneakersComments`
 --
 ALTER TABLE `sneakersComments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `sneakersImages`
 --
 ALTER TABLE `sneakersImages`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `sneakersInfo`
 --
 ALTER TABLE `sneakersInfo`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
